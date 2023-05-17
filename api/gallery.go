bd159e01ff2ac3c7e230162d0e36c6cee93e513e
mudler <mudler@mocaccino.org>
2023-05-18 00:50:50 +0200

    refactor: return url in 'status'

--- api/gallery.go
+++ api/gallery.go
@@ -133,8 +133,8 @@ func applyModelGallery(modelPath string, cm *ConfigMerger, g chan galleryOp) fun
 			id:  uuid.String(),
 		}
 		return c.JSON(struct {
-			ID  string `json:"uid"`
-			URL string `json:"url"`
-		}{ID: uuid.String(), URL: c.BaseURL() + "/models/jobs/" + uuid.String()})
+			ID        string `json:"uid"`
+			StatusURL string `json:"status"`
+		}{ID: uuid.String(), StatusURL: c.BaseURL() + "/models/jobs/" + uuid.String()})
 	}
 }
