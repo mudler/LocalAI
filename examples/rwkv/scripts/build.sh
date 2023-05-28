bda8b36a1c46348adcb0969ac4dddcd49d940f0c
mudler <mudler@mocaccino.org>
2023-05-28 17:46:34 +0200

    Update examples

--- examples/rwkv/scripts/build.sh
+++ examples/rwkv/scripts/build.sh
@@ -8,4 +8,4 @@ FILENAME=$(basename $URL)
 wget -nc $URL -O /build/$FILENAME
 
 python3 /build/rwkv.cpp/rwkv/convert_pytorch_to_ggml.py /build/$FILENAME /build/float-model float16
-python3 /build/rwkv.cpp/rwkv/quantize.py /build/float-model $OUT Q4_2
+python3 /build/rwkv.cpp/rwkv/quantize.py /build/float-model $OUT Q4_0
