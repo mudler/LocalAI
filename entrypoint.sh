48f526622c374f8c95100d66d18ac8a0a94a2d1e
finger42 <67188554+finger42@users.noreply.github.com>
2023-07-23 14:26:09 +0000

    Added CPU information to entrypoint.sh
    This should help to show the CPU capabilities
    regarding AVX, AVX2 and AVX512.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,25 @@ else
 	echo "see the documentation at: https://localai.io/basics/build/index.html"
 	echo "Note: See also https://github.com/go-skynet/LocalAI/issues/288"
 	echo "@@@@@"
+	echo "CPU info:"
+	grep -e "model\sname" /proc/cpuinfo | head -1
+	grep -e "flags" /proc/cpuinfo | head -1
+	if grep -q -e "\savx\s" /proc/cpuinfo ; then
+		echo "CPU:    AVX    found OK"
+	else
+		echo "CPU: no AVX    found"
+	fi
+	if grep -q -e "\savx2\s" /proc/cpuinfo ; then
+		echo "CPU:    AVX2   found OK"
+	else
+		echo "CPU: no AVX2   found"
+	fi
+	if grep -q -e "\savx512" /proc/cpuinfo ; then
+		echo "CPU:    AVX512 found OK"
+	else
+		echo "CPU: no AVX512 found"
+	fi
+	echo "@@@@@"
 fi
 
 ./local-ai "$@"
