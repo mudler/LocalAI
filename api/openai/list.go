2969b3f935fa040696dfc1053172af3e1c793f85
Dave Lee <dave@gray101.com>
2023-07-28 01:52:14 -0400

    filter query string parameter

--- api/openai/list.go
+++ api/openai/list.go
@@ -1,6 +1,8 @@
 package openai
 
 import (
+	"regexp"
+
 	config "github.com/go-skynet/LocalAI/api/config"
 	model "github.com/go-skynet/LocalAI/pkg/model"
 	"github.com/gofiber/fiber/v2"
@@ -15,14 +17,39 @@ func ListModelsEndpoint(loader *model.ModelLoader, cm *config.ConfigLoader) func
 		var mm map[string]interface{} = map[string]interface{}{}
 
 		dataModels := []OpenAIModel{}
-		for _, m := range models {
-			mm[m] = nil
-			dataModels = append(dataModels, OpenAIModel{ID: m, Object: "model"})
+
+		skipBinIfModelConfigured := false
+		var filterFn func(name string) bool
+		filter := c.Query("filter")
+		if filter == "" {
+			// If filter param isn't specified at all, take our best guess at presenting a useful list to our users.
+			// 1) Remove any raw model files from the list that are already configured
+			skipBinIfModelConfigured = true
+			// 2) Don't filter out anything else
+			filterFn = func(_ string) bool { return true }
+		} else {
+			// If filter _IS_ specified, we should use it to create the filterFn
+			rxp, err := regexp.Compile(filter)
+			if err != nil {
+				return nil
+			}
+			filterFn = func(name string) bool {
+				return rxp.MatchString(name)
+			}
 		}
 
-		for _, k := range cm.ListConfigs() {
-			if _, exists := mm[k]; !exists {
-				dataModels = append(dataModels, OpenAIModel{ID: k, Object: "model"})
+		for _, c := range cm.GetAllConfigs() {
+			if skipBinIfModelConfigured {
+				mm[c.Model] = nil
+			}
+			if filterFn(c.Name) {
+				dataModels = append(dataModels, OpenAIModel{ID: c.Name, Object: "model"})
+			}
+		}
+
+		for _, m := range models {
+			if _, exists := mm[m]; !exists && filterFn(m) {
+				dataModels = append(dataModels, OpenAIModel{ID: m, Object: "model"})
 			}
 		}
 
