3eb1c78714c621fba32fe9fbdd120218cc4b0739
Aisuko <urakiny@gmail.com>
2023-10-10 00:43:44 +0000

    Add seperate env for vall-x e
    Signed-off-by: GitHub <noreply@github.com>

---
+++ extra/grpc/vall-e-x/run.sh
@@ -0,0 +1,10 @@
+##
+## A bash script wrapper that runs the ttsvalle server with conda
+
+# Activate conda environment
+source activate ttsvalle
+
+# get the directory where the bash script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+python $DIR/ttvalle.py
