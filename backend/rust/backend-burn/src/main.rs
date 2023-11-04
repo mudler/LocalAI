660cc49746df7d8818a015f09d7fb949ff2b085f
Aisuko <urakiny@gmail.com>
2023-11-04 13:11:22 +1100

    Add check memory feature
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- backend/rust/backend-burn/src/main.rs
+++ backend/rust/backend-burn/src/main.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
 
 use bunker::pb::Result as PbResult;
 use bunker::pb::{
-    EmbeddingResult, GenerateImageRequest, HealthMessage, ModelOptions, PredictOptions, Reply,
-    StatusResponse, TokenizationResponse, TranscriptRequest, TranscriptResult, TtsRequest,
+    EmbeddingResult, GenerateImageRequest, HealthMessage, MemoryUsageData, ModelOptions,
+    PredictOptions, Reply, StatusResponse, TokenizationResponse, TranscriptRequest,
+    TranscriptResult, TtsRequest,
 };
 
 use bunker::BackendService;
@@ -13,6 +15,10 @@ use tonic::{Request, Response, Status};
 use async_trait::async_trait;
 
 use tracing::{event, span, Level};
+use tracing_subscriber::filter::LevelParseError;
+
+use std::fs;
+use std::process::{Command,id};
 
 use models::*;
 // implement BackendService trait in bunker
@@ -115,7 +121,49 @@ impl BackendService for BurnBackend {
         &self,
         request: Request<HealthMessage>,
     ) -> Result<Response<StatusResponse>, Status> {
-        todo!()
+        
+        // Here we do not need to cover the windows platform
+        let mut breakdown = HashMap::new();
+        let mut memory_usage: u64=0;
+
+        #[cfg(target_os = "linux")]
+        {
+            let pid =id();
+            let stat = fs::read_to_string(format!("/proc/{}/stat", pid)).expect("Failed to read stat file");
+
+            let stats: Vec<&str> = stat.split_whitespace().collect();
+            memory_usage = stats[23].parse::<u64>().expect("Failed to parse RSS");
+        }
+
+        #[cfg(target_os="macos")]
+        {
+            let output=Command::new("ps")
+            .arg("-p")
+            .arg(id().to_string())
+            .arg("-o")
+            .arg("rss=")
+            .output()
+            .expect("failed to execute process");
+    
+            memory_usage = String::from_utf8_lossy(&output.stdout)
+            .trim()
+            .parse::<u64>()
+            .expect("Failed to parse memory usage");
+
+        }
+        breakdown.insert("RSS".to_string(), memory_usage);
+
+        let memory_usage = Option::from(MemoryUsageData {
+            total: memory_usage,
+            breakdown,
+        });
+
+        let reponse = StatusResponse {
+            state: 0, //TODO: add state https://github.com/mudler/LocalAI/blob/9b17af18b3aa0c3cab16284df2d6f691736c30c1/pkg/grpc/proto/backend.proto#L188C9-L188C9
+            memory: memory_usage,
+        };
+
+        Ok(Response::new(reponse))
     }
 }
 
