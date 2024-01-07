edcdd4715e981d8bd213017d10325bc6e03a6420
Ettore Di Giacinto <mudler@localai.io>
2024-01-07 09:55:15 +0100

    images: cleanup pip cache after install

--- backend/python/exllama2/install.sh
+++ backend/python/exllama2/install.sh
@@ -12,3 +12,7 @@ echo $CONDA_PREFIX
 git clone https://github.com/turboderp/exllamav2 $CONDA_PREFIX/exllamav2 && pushd $CONDA_PREFIX/exllamav2 && pip install -r requirements.txt && popd
 
 cp -rfv $CONDA_PREFIX/exllamav2/* ./  
+
+if [ "$PIP_CACHE_PURGE" = true ] ; then
+    pip cache purge
+fi
