50e23dd1180b9950bd896e904e20119b33045434
Ettore Di Giacinto <mudler@localai.io>
2024-02-23 18:44:51 +0100

    feat(intel): add diffusers support

--- backend/python/diffusers/run.sh
+++ backend/python/diffusers/run.sh
@@ -8,6 +8,11 @@ export PATH=$PATH:/opt/conda/bin
 # Activate conda environment
 source activate diffusers
 
+if [ -d "/opt/intel" ]; then
+    source /opt/intel/oneapi/compiler/latest/env/vars.sh
+    source /opt/intel/oneapi/mkl/latest/env/vars.sh
+fi
+
 # get the directory where the bash script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
