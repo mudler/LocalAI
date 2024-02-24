0c23fbafbc674749eb3dcdeb5bb58f2aa0aeedf8
Ettore Di Giacinto <mudler@localai.io>
2024-02-24 23:51:23 +0100

    Manually install deps

--- backend/python/diffusers/install.sh
+++ backend/python/diffusers/install.sh
@@ -14,6 +14,16 @@ else
     echo "Virtual environment already exists."
 fi
 
+if [ -d "/opt/intel" ]; then
+    pip install --upgrade google-api-python-client
+    pip install --upgrade grpcio
+    pip install --upgrade grpcio-tools
+    pip install diffusers==0.24.0
+    pip install transformers>=4.25.1
+    pip install accelerate
+    pip install compel==2.0.2
+fi
+
 if [ "$PIP_CACHE_PURGE" = true ] ; then
     export PATH=$PATH:/opt/conda/bin
 
