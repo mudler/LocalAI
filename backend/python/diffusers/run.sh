5c9fa5dc0bdd820bbae3319abb3042f5b04cbb45
Ettore Di Giacinto <mudler@localai.io>
2024-03-05 19:53:47 +0100

    Prefer python3

--- backend/python/diffusers/run.sh
+++ backend/python/diffusers/run.sh
@@ -16,16 +16,16 @@ fi
 # get the directory where the bash script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Intel image: If there is no "python" command, try "python3"
-if ! [ -x "$(command -v python)" ]; then
-  if [ -x "$(command -v python3)" ]; then
-    export PYTHON=python3
+# Intel image: If there is no "python3" command, try "python"
+if ! [ -x "$(command -v python3)" ]; then
+  if [ -x "$(command -v python)" ]; then
+    export PYTHON=python
   else
     echo 'Error: python is not installed.' >&2
     exit 1
   fi
 else
-  export PYTHON=python
+  export PYTHON=python3
 fi
 
 $PYTHON $DIR/backend_diffusers.py $@
