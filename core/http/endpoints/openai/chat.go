19a6c8bcc4edf78a9718d696daa8fa88961477c3
Dave Lee <dave@gray101.com>
2024-03-12 02:09:07 -0400

    sync the end?

--- core/http/endpoints/openai/chat.go
+++ core/http/endpoints/openai/chat.go
@@ -97,6 +97,7 @@ func ChatEndpoint(fce *fiberContext.FiberContextExtractor, oais *services.OpenAI
 				w.WriteString("data: [DONE]\n\n")
 				w.Flush()
 			}))
+			<-finalResultChannel
 			return nil
 		}
 
