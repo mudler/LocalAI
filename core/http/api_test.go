b3ece0ec945a47d8510596f1ddf0c288e3004447
Dave Lee <dave@gray101.com>
2024-03-15 13:33:22 -0400

    test with gguf instead of ggml. Updates testPrompt to match? Adds debugging line to Dockerfile that I've found helpful recently.

--- core/http/api_test.go
+++ core/http/api_test.go
@@ -30,14 +30,10 @@ import (
 	"github.com/sashabaranov/go-openai/jsonschema"
 )
 
-const testPrompt = `### System:
-You are an AI assistant that follows instruction extremely well. Help as much as you can.
-
-### User:
-
-Can you help rephrasing sentences?
-
-### Response:`
+const testPrompt = `<|im_start|>system
+You are an AI assistant that follows instruction extremely well. Help as much as you can.<|im_end|>
+<|im_start|>user
+`
 
 type modelApplyRequest struct {
 	ID        string                 `json:"id"`
