61ead2cae97dc276aa4be039d35533e2a9301265
Ettore Di Giacinto <mudler@localai.io>
2024-04-13 13:42:04 +0200

    feat(parler-tts): Add new backend
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/parler-tts/run.sh
+++ backend/python/parler-tts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ##
-## A bash script wrapper that runs the transformers-musicgen server with conda
+## A bash script wrapper that runs the parler-tts server with conda
 
-echo "Launching gRPC server for transformers-musicgen"
+echo "Launching gRPC server for parler-tts"
 
 export PATH=$PATH:/opt/conda/bin
 
@@ -13,4 +13,4 @@ source activate transformers
 # get the directory where the bash script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python $DIR/transformers_server.py $@
+python $DIR/parler_tts_server.py $@
