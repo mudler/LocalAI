f41b7e1feb547705c468cb404855fedd86819d42
Ettore Di Giacinto <mudler@localai.io>
2024-04-13 15:17:47 +0200

    feat(parler-tts): add parler conda env
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/parler-tts/run.sh
+++ backend/python/parler-tts/run.sh
@@ -8,7 +8,7 @@ echo "Launching gRPC server for parler-tts"
 export PATH=$PATH:/opt/conda/bin
 
 # Activate conda environment
-source activate transformers
+source activate parler
 
 # get the directory where the bash script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
