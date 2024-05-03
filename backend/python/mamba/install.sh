31aba66895932c719f4aeb678b97a8b6b4468172
Chris Jowett <421501+cryptk@users.noreply.github.com>
2024-05-03 12:41:25 -0500

    feat: migrate mamba to uv
    Signed-off-by: Chris Jowett <421501+cryptk@users.noreply.github.com>

--- backend/python/mamba/install.sh
+++ backend/python/mamba/install.sh
@@ -8,14 +8,27 @@ if [ "$BUILD_TYPE" != "cublas" ]; then
     exit 0
 fi
 
-export PATH=$PATH:/opt/conda/bin
+MY_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
-# Activate conda environment
-source activate transformers
+python -m venv ${MY_DIR}/venv
+source ${MY_DIR}/venv/bin/activate
 
-echo $CONDA_PREFIX
+# mabma does not specify it's build dependencies per PEP517, so we need to disable build isolation
+# this also means that we need to install the basic build dependencies into the venv ourselves
+# https://github.com/Dao-AILab/causal-conv1d/issues/24
+uv pip install --requirement ${MY_DIR}/requirements-install.txt
 
-pip install causal-conv1d==1.0.0 mamba-ssm==1.0.1
+uv pip install --no-build-isolation --requirement ${MY_DIR}/requirements.txt
+
+if [ -f "requirements-${BUILD_TYPE}.txt" ]; then
+    uv pip install --requirement ${MY_DIR}/requirements-${BUILD_TYPE}.txt
+fi
+
+if [ -d "/opt/intel" ]; then
+    # Intel GPU: If the directory exists, we assume we are using the Intel image
+    # https://github.com/intel/intel-extension-for-pytorch/issues/538
+    uv pip install --index-url https://pytorch-extension.intel.com/release-whl/stable/xpu/us/ --requirement ${MY_DIR}/requirements-intel.txt
+fi
 
 if [ "$PIP_CACHE_PURGE" = true ] ; then
     pip cache purge
