84e2407afaf113345eba598d36609192d90f881d
Ettore Di Giacinto <mudler@users.noreply.github.com>
2024-05-14 09:39:20 +0200

    feat(functions): allow to set JSON matcher (#2319)
    Signed-off-by: mudler <mudler@localai.io>

--- core/http/endpoints/openai/chat.go
+++ core/http/endpoints/openai/chat.go
@@ -81,7 +81,7 @@ func ChatEndpoint(cl *config.BackendConfigLoader, ml *model.ModelLoader, startup
 			}
 			responses <- initialMessage
 
-			result, err := handleQuestion(config, req, ml, startupOptions, results, prompt)
+			result, err := handleQuestion(config, req, ml, startupOptions, results, result, prompt)
 			if err != nil {
 				log.Error().Err(err).Msg("error handling question")
 				return
@@ -470,7 +470,7 @@ func ChatEndpoint(cl *config.BackendConfigLoader, ml *model.ModelLoader, startup
 
 				switch {
 				case noActionsToRun:
-					result, err := handleQuestion(config, input, ml, startupOptions, results, predInput)
+					result, err := handleQuestion(config, input, ml, startupOptions, results, s, predInput)
 					if err != nil {
 						log.Error().Err(err).Msg("error handling question")
 						return
@@ -550,7 +550,14 @@ func ChatEndpoint(cl *config.BackendConfigLoader, ml *model.ModelLoader, startup
 	}
 }
 
-func handleQuestion(config *config.BackendConfig, input *schema.OpenAIRequest, ml *model.ModelLoader, o *config.ApplicationConfig, funcResults []functions.FuncCallResults, prompt string) (string, error) {
+func handleQuestion(config *config.BackendConfig, input *schema.OpenAIRequest, ml *model.ModelLoader, o *config.ApplicationConfig, funcResults []functions.FuncCallResults, result, prompt string) (string, error) {
+
+	if len(funcResults) == 0 && result != "" {
+		log.Debug().Msgf("nothing function results but we had a message from the LLM")
+
+		return result, nil
+	}
+
 	log.Debug().Msgf("nothing to do, computing a reply")
 	arg := ""
 	if len(funcResults) > 0 {
