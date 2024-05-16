ea9a3cb29f8b3d35c851a1f3d462063c92d656fb
mudler <mudler@localai.io>
2024-05-17 01:25:00 +0200

    feat(llama.cpp): Enable decentralized, distributed inference
    As https://github.com/mudler/LocalAI/pull/2324 introduced distributed inferencing thanks to
    @rgerganov implementation in https://github.com/ggerganov/llama.cpp/pull/6829 in upstream llama.cpp, now
    it is possible to distribute the workload to remote llama.cpp gRPC server.
    
    This changeset now uses mudler/edgevpn to establish a secure, distributed network between the nodes using a shared token.
    The token is generated automatically when starting the server with the `--p2p` flag, and can be used by starting the workers
    with `local-ai worker p2p-llama-cpp-rpc` by passing the token via environment variable (TOKEN) or with args (--token).
    
    As per how mudler/edgevpn works, a network is established between the server and the workers with dht and mdns discovery protocols,
    the llama.cpp rpc server is automatically started and exposed to the underlying p2p network so the API server can connect on.
    
    When the HTTP server is started, it will discover the workers in the network and automatically create the port-forwards to the service locally.
    Then llama.cpp is configured to use the services.
    
    This feature is behind the "p2p" GO_FLAGS
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/cli/worker/worker_llamacpp.go
+++ core/cli/worker/worker_llamacpp.go
@@ -1,24 +1,30 @@
-package cli
+package worker
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 
+	cliContext "github.com/go-skynet/LocalAI/core/cli/context"
 	"github.com/go-skynet/LocalAI/pkg/assets"
 	"github.com/rs/zerolog/log"
 )
 
-type LLAMACPPWorkerCMD struct {
-	Args              []string `arg:"" optional:"" name:"models" help:"Worker arguments: host port"`
-	BackendAssetsPath string   `env:"LOCALAI_BACKEND_ASSETS_PATH,BACKEND_ASSETS_PATH" type:"path" default:"/tmp/localai/backend_data" help:"Path used to extract libraries that are required by some of the backends in runtime" group:"storage"`
+type LLamaCPP struct {
+	Args        []string `arg:"" optional:"" name:"models" help:"Model configuration URLs to load"`
+	WorkerFlags `embed:""`
 }
 
-func (r *LLAMACPPWorkerCMD) Run(ctx *Context) error {
+func (r *LLamaCPP) Run(ctx *cliContext.Context) error {
 	// Extract files from the embedded FS
 	err := assets.ExtractFiles(ctx.BackendAssets, r.BackendAssetsPath)
 	log.Debug().Msgf("Extracting backend assets files to %s", r.BackendAssetsPath)
 	if err != nil {
 		log.Warn().Msgf("Failed extracting backend assets files: %s (might be required for some backends to work properly, like gpt4all)", err)
+	}
+
+	if len(os.Args) < 4 {
+		return fmt.Errorf("usage: local-ai worker llama-cpp-rpc -- <llama-rpc-server-args>")
 	}
 
 	return syscall.Exec(
@@ -32,6 +38,6 @@ func (r *LLAMACPPWorkerCMD) Run(ctx *Context) error {
 				r.BackendAssetsPath,
 				"util",
 				"llama-cpp-rpc-server",
-			)}, r.Args...),
+			)}, os.Args[4:]...),
 		os.Environ())
 }
