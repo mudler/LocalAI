20c8a6af6185d2bf15eb4c4ee0ed14c4c206127c
Lenaxia <github@47north.lat>
2024-05-18 17:57:31 +0000

    Adding tests for the regex replace in ParseFunctionCall
    Signed-off-by: Lenaxia <github@47north.lat>

--- pkg/functions/parse_test.go
+++ pkg/functions/parse_test.go
@@ -110,4 +110,45 @@ var _ = Describe("LocalAI function parse tests", func() {
 			Expect(results[0].Arguments).To(Equal(`{"x":5,"y":3}`))
 		})
 	})
+
+	Context("when using ReplaceResults to clean up input", func() {
+		It("should replace text before and after JSON blob", func() {
+			input := `
+Some text before the JSON
+{"function": "add", "arguments": {"x": 5, "y": 3}}
+Some text after the JSON
+`
+			functionConfig.ReplaceResults = map[string]string{
+				`(?s)^[^\\{]*(?=\\{)`: "",
+				`(?s)\\}[^{}]+$`:      "",
+			}
+
+			results := ParseFunctionCall(input, functionConfig)
+			Expect(results).To(HaveLen(1))
+			Expect(results[0].Name).To(Equal("add"))
+			Expect(results[0].Arguments).To(Equal(`{"x":5,"y":3}`))
+		})
+
+	Context("when using ReplaceResults to clean up input from JSON array", func() {
+		It("should replace text before and after array JSON array", func() {
+			input := `
+Some text before the JSON
+[{"function": "add", "arguments": {"x": 5, "y": 3}}, {"function": "subtract", "arguments": {"x": 10, "y": 7}}]
+Some text after the JSON
+`
+			functionConfig.FunctionName = true
+			functionConfig.ReplaceResults = map[string]string{
+				`(?s)^[^\\[]{*(?=\\[)`: "",
+				`(?s)\\][^]]+$`:         "",
+			}
+
+			results := ParseFunctionCall(input, functionConfig)
+			Expect(results).To(HaveLen(2))
+			Expect(results[0].Name).To(Equal("add"))
+			Expect(results[0].Arguments).To(Equal(`{"x":5,"y":3}`))
+			Expect(results[1].Name).To(Equal("subtract"))
+			Expect(results[1].Arguments).To(Equal(`{"x":10,"y":7}`))
+		})
+	})
+	})
 })
