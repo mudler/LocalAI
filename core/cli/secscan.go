4159c2646d5bc9e5c1fe261b536b50d0f8c30ff7
Dave Lee <dave@gray101.com>
2024-06-13 19:08:55 -0400

    add a param to scan specific urls - useful for debugging
    Signed-off-by: Dave Lee <dave@gray101.com>

--- core/cli/secscan.go
+++ core/cli/secscan.go
@@ -2,29 +2,44 @@ package cli
 
 import (
 	"encoding/json"
+	"errors"
 
 	cliContext "github.com/go-skynet/LocalAI/core/cli/context"
+	"github.com/go-skynet/LocalAI/pkg/downloader"
 	"github.com/go-skynet/LocalAI/pkg/gallery"
 	"github.com/rs/zerolog/log"
 )
 
 type SecScanCLI struct {
-	ModelsPath string `env:"LOCALAI_MODELS_PATH,MODELS_PATH" type:"path" default:"${basepath}/models" help:"Path containing models used for inferencing" group:"storage"`
-	Galleries  string `env:"LOCALAI_GALLERIES,GALLERIES" help:"JSON list of galleries" group:"models" default:"${galleries}"`
+	ModelsPath string   `env:"LOCALAI_MODELS_PATH,MODELS_PATH" type:"path" default:"${basepath}/models" help:"Path containing models used for inferencing" group:"storage"`
+	Galleries  string   `env:"LOCALAI_GALLERIES,GALLERIES" help:"JSON list of galleries" group:"models" default:"${galleries}"`
+	ToScan     []string `arg:""`
 }
 
 func (sscli *SecScanCLI) Run(ctx *cliContext.Context) error {
 	log.Info().Msg("LocalAI Security Scanner - This is BEST EFFORT functionality! Currently limited to huggingface models!")
-	var galleries []gallery.Gallery
-	if err := json.Unmarshal([]byte(sscli.Galleries), &galleries); err != nil {
-		log.Error().Err(err).Msg("unable to load galleries")
-	}
+	if len(sscli.ToScan) == 0 {
+		var galleries []gallery.Gallery
+		if err := json.Unmarshal([]byte(sscli.Galleries), &galleries); err != nil {
+			log.Error().Err(err).Msg("unable to load galleries")
+		}
 
-	err := gallery.SafetyScanGalleryModels(galleries, sscli.ModelsPath)
-	if err == nil {
-		log.Info().Msg("No security warnings were detected for your installed models. Please note that this is a BEST EFFORT tool, and all issues may not be detected.")
+		err := gallery.SafetyScanGalleryModels(galleries, sscli.ModelsPath)
+		if err == nil {
+			log.Info().Msg("No security warnings were detected for your installed models. Please note that this is a BEST EFFORT tool, and all issues may not be detected.")
+		} else {
+			log.Error().Err(err).Msg("! WARNING ! A known-vulnerable model is installed!")
+		}
+		return err
 	} else {
-		log.Error().Err(err).Msg("! WARNING ! A known-vulnerable model is installed!")
+		for _, uri := range sscli.ToScan {
+			scanResults, err := downloader.HuggingFaceScan(uri)
+			if err != nil && !errors.Is(err, downloader.NonHuggingFaceFileError) {
+				log.Error().Err(err).Strs("clamAV", scanResults.ClamAVInfectedFiles).Strs("pickles", scanResults.DangerousPickles).Msg("! WARNING ! A known-vulnerable model is installed!")
+				return err
+			}
+		}
+		log.Info().Msg("No security warnings were detected for your installed models. Please note that this is a BEST EFFORT tool, and all issues may not be detected.")
+		return nil
 	}
-	return err
 }
