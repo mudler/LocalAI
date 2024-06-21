f6a40f045e511fc1973599e9e1c266585ca901b3
Ettore Di Giacinto <mudler@localai.io>
2024-06-21 19:47:53 +0200

    fixup tests
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- pkg/oci/blob_test.go
+++ pkg/oci/blob_test.go
@@ -1,6 +1,8 @@
 package oci_test
 
 import (
+	"os"
+
 	. "github.com/go-skynet/LocalAI/pkg/oci" // Update with your module path
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -9,7 +11,10 @@ import (
 var _ = Describe("OCI", func() {
 	Context("pulling images", func() {
 		It("should fetch blobs correctly", func() {
-			err := FetchImageBlob("registry.ollama.ai/library/gemma", "sha256:c1864a5eb19305c40519da12cc543519e48a0697ecd30e15d5ac228644957d12", "/tmp/foo", nil)
+			f, err := os.CreateTemp("", "ollama")
+			Expect(err).NotTo(HaveOccurred())
+			defer os.RemoveAll(f.Name())
+			err = FetchImageBlob("registry.ollama.ai/library/gemma", "sha256:c1864a5eb19305c40519da12cc543519e48a0697ecd30e15d5ac228644957d12", f.Name(), nil)
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})
--- pkg/oci/ollama_test.go
+++ pkg/oci/ollama_test.go
@@ -1,6 +1,8 @@
 package oci_test
 
 import (
+	"os"
+
 	. "github.com/go-skynet/LocalAI/pkg/oci" // Update with your module path
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -8,8 +10,11 @@ import (
 
 var _ = Describe("OCI", func() {
 	Context("ollama", func() {
-		FIt("pulls model files", func() {
-			err := OllamaFetchModel("gemma:2b", "/tmp/foo", nil)
+		It("pulls model files", func() {
+			f, err := os.CreateTemp("", "ollama")
+			Expect(err).NotTo(HaveOccurred())
+			defer os.RemoveAll(f.Name())
+			err = OllamaFetchModel("gemma:2b", f.Name(), nil)
 			Expect(err).NotTo(HaveOccurred())
 		})
 	})
