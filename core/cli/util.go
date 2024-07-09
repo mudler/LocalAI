5e15661888b6f5303a136129cfd73ca7af2aec90
Dave Lee <dave@gray101.com>
2024-07-09 13:25:40 -0400

    forgot secscan could accept multiple URL at once
    Signed-off-by: Dave Lee <dave@gray101.com>

--- core/cli/util.go
+++ core/cli/util.go
@@ -83,13 +83,17 @@ func (hfscmd *HFScanCMD) Run(ctx *cliContext.Context) error {
 		}
 		return err
 	} else {
+		var errs error = nil
 		for _, uri := range hfscmd.ToScan {
 			log.Info().Str("uri", uri).Msg("scanning specific uri")
 			scanResults, err := downloader.HuggingFaceScan(uri)
 			if err != nil && !errors.Is(err, downloader.ErrNonHuggingFaceFile) {
 				log.Error().Err(err).Strs("clamAV", scanResults.ClamAVInfectedFiles).Strs("pickles", scanResults.DangerousPickles).Msg("! WARNING ! A known-vulnerable model is included in this repo!")
-				return err
+				errs = errors.Join(errs, err)
 			}
+		}
+		if errs != nil {
+			return errs
 		}
 		log.Info().Msg("No security warnings were detected for your installed models. Please note that this is a BEST EFFORT tool, and all issues may not be detected.")
 		return nil
