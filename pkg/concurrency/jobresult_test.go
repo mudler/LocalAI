4edf4949ef4df040184dee0c5dce588f7f8008ad
Dave Lee <dave@gray101.com>
2024-07-17 15:45:01 -0400

    timeout tests and style fix
    Signed-off-by: Dave Lee <dave@gray101.com>

--- pkg/concurrency/jobresult_test.go
+++ pkg/concurrency/jobresult_test.go
@@ -15,40 +15,65 @@ var _ = Describe("pkg/concurrency unit tests", func() {
 		jr, wjr := NewJobResult[string, string]("foo")
 		Expect(jr).ToNot(BeNil())
 		Expect(wjr).ToNot(BeNil())
-		stallChannel := make(chan struct{})
-		go func(jr *JobResult[string, string]) {
-			resPtr, err := jr.Wait(context.Background())
-			Expect(err).To(BeNil())
-			Expect(jr.Request).ToNot(BeNil())
-			Expect(*jr.Request()).To(Equal("foo"))
-			Expect(resPtr).ToNot(BeNil())
-			Expect(*resPtr).To(Equal("bar"))
-			close(stallChannel)
-		}(jr)
+
 		go func(wjr *WritableJobResult[string, string]) {
 			time.Sleep(time.Second * 5)
 			wjr.SetResult("bar", nil)
 		}(wjr)
-		<-stallChannel
+
+		resPtr, err := jr.Wait(context.Background())
+		Expect(err).To(BeNil())
+		Expect(jr.Request).ToNot(BeNil())
+		Expect(*jr.Request()).To(Equal("foo"))
+		Expect(resPtr).ToNot(BeNil())
+		Expect(*resPtr).To(Equal("bar"))
+
 	})
 
 	It("can be used to recieve an error across goroutines", func() {
 		jr, wjr := NewJobResult[string, string]("foo")
 		Expect(jr).ToNot(BeNil())
 		Expect(wjr).ToNot(BeNil())
-		stallChannel := make(chan struct{})
-		go func(jr *JobResult[string, string]) {
-			_, err := jr.Wait(context.Background())
-			Expect(jr.Request).To(BeNil())
-			Expect(*jr.Request()).To(Equal("foo"))
-			Expect(err).ToNot(BeNil())
-			Expect(err).To(MatchError("test"))
-			close(stallChannel)
-		}(jr)
+
 		go func(wjr *WritableJobResult[string, string]) {
 			time.Sleep(time.Second * 5)
 			wjr.SetResult("", fmt.Errorf("test"))
 		}(wjr)
-		<-stallChannel
+
+		_, err := jr.Wait(context.Background())
+		Expect(jr.Request).To(BeNil())
+		Expect(*jr.Request()).To(Equal("foo"))
+		Expect(err).ToNot(BeNil())
+		Expect(err).To(MatchError("test"))
+	})
+
+	It("can properly handle timeouts", func() {
+		jr, wjr := NewJobResult[string, string]("foo")
+		Expect(jr).ToNot(BeNil())
+		Expect(wjr).ToNot(BeNil())
+
+		go func(wjr *WritableJobResult[string, string]) {
+			time.Sleep(time.Second * 5)
+			wjr.SetResult("", fmt.Errorf("test"))
+		}(wjr)
+
+		timeout1s, c1 := context.WithTimeoutCause(context.Background(), time.Second, fmt.Errorf("timeout"))
+		timeout10s, c2 := context.WithTimeoutCause(context.Background(), time.Second*10, fmt.Errorf("timeout"))
+
+		_, err := jr.Wait(timeout1s)
+		Expect(jr.Request).To(BeNil())
+		Expect(*jr.Request()).To(Equal("foo"))
+		Expect(err).ToNot(BeNil())
+		Expect(err).To(MatchError("timeout"))
+
+		resPtr, err := jr.Wait(timeout10s)
+		Expect(err).To(BeNil())
+		Expect(*jr.Request()).To(Equal("foo"))
+		Expect(resPtr).ToNot(BeNil())
+		Expect(*resPtr).To(Equal("bar"))
+
+		// Is this needed? Cleanup Either Way.
+		c1()
+		c2()
 	})
 })
