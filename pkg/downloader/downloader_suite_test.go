d3b220e4d23b1848669b7ad73b52e6ec0f712da0
Dave Lee <dave@gray101.com>
2024-07-17 16:26:18 -0400

    add missing suite test files preventing tests from running
    Signed-off-by: Dave Lee <dave@gray101.com>

---
+++ pkg/downloader/downloader_suite_test.go
@@ -0,0 +1,13 @@
+package downloader
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGrammar(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Grammar test suite")
+}
