12e5a0390e03ddb6937cb5392fa7816b14699e87
Ettore Di Giacinto <mudler@localai.io>
2024-08-06 12:08:17 +0200

    Wire up a simple explorer DB
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/cli/explorer.go
+++ core/cli/explorer.go
@@ -2,15 +2,22 @@ package cli
 
 import (
 	cliContext "github.com/mudler/LocalAI/core/cli/context"
+	"github.com/mudler/LocalAI/core/explorer"
 	"github.com/mudler/LocalAI/core/http"
 )
 
 type ExplorerCMD struct {
-	Address string `env:"LOCALAI_ADDRESS,ADDRESS" default:":8080" help:"Bind address for the API server" group:"api"`
+	Address      string `env:"LOCALAI_ADDRESS,ADDRESS" default:":8080" help:"Bind address for the API server" group:"api"`
+	PoolDatabase string `env:"LOCALAI_POOL_DATABASE,POOL_DATABASE" default:"" help:"Path to the pool database" group:"api"`
 }
 
-func (explorer *ExplorerCMD) Run(ctx *cliContext.Context) error {
-	appHTTP := http.Explorer()
+func (e *ExplorerCMD) Run(ctx *cliContext.Context) error {
 
-	return appHTTP.Listen(explorer.Address)
+	db, err := explorer.NewDatabase(e.PoolDatabase)
+	if err != nil {
+		return err
+	}
+	appHTTP := http.Explorer(db)
+
+	return appHTTP.Listen(e.Address)
 }
--- core/http/explorer.go
+++ core/http/explorer.go
@@ -2,20 +2,22 @@ package http
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/mudler/LocalAI/core/explorer"
 	"github.com/mudler/LocalAI/core/http/routes"
 )
 
-func Explorer() *fiber.App {
+func Explorer(db *explorer.Database) *fiber.App {
 
 	fiberCfg := fiber.Config{
 		Views: renderEngine(),
 		// We disable the Fiber startup message as it does not conform to structured logging.
 		// We register a startup log line with connection information in the OnListen hook to keep things user friendly though
-		DisableStartupMessage: true,
+		DisableStartupMessage: false,
 		// Override default error handler
 	}
 
 	app := fiber.New(fiberCfg)
+
 	routes.RegisterExplorerRoutes(app)
 
 	return app
