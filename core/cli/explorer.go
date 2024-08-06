884d50f3b3d9238a49e9a47355399300e5c6b6db
Ettore Di Giacinto <mudler@localai.io>
2024-08-06 11:53:21 +0200

    Fixups
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/cli/explorer.go
+++ core/cli/explorer.go
@@ -3,7 +3,6 @@ package cli
 import (
 	cliContext "github.com/mudler/LocalAI/core/cli/context"
 	"github.com/mudler/LocalAI/core/http"
-	"github.com/rs/zerolog/log"
 )
 
 type ExplorerCMD struct {
@@ -11,11 +10,7 @@ type ExplorerCMD struct {
 }
 
 func (explorer *ExplorerCMD) Run(ctx *cliContext.Context) error {
-	appHTTP, err := http.App(cl, ml, options)
-	if err != nil {
-		log.Error().Err(err).Msg("error during HTTP App construction")
-		return err
-	}
+	appHTTP := http.Explorer()
 
 	return appHTTP.Listen(explorer.Address)
 }
