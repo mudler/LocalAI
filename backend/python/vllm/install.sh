2553de01878b76f94daa29d0cb135c0bc0730ea4
Ettore Di Giacinto <mudler@users.noreply.github.com>
2024-10-04 23:42:05 +0200

    feat(vllm): add support for image-to-text and video-to-text (#3729)
    * feat(vllm): add support for image-to-text
    
    Related to https://github.com/mudler/LocalAI/issues/3670
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * feat(vllm): add support for video-to-text
    
    Closes: https://github.com/mudler/LocalAI/issues/2318
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * feat(vllm): support CPU installations
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * feat(vllm): add bnb
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * chore: add docs reference
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Apply suggestions from code review
    
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

--- backend/python/vllm/install.sh
+++ backend/python/vllm/install.sh
@@ -13,4 +13,18 @@ if [ "x${BUILD_PROFILE}" == "xintel" ]; then
     EXTRA_PIP_INSTALL_FLAGS+=" --upgrade --index-strategy=unsafe-first-match"
 fi
 
-installRequirements
+if [ "x${BUILD_TYPE}" == "x" ]; then
+        ensureVenv
+        # https://docs.vllm.ai/en/v0.6.1/getting_started/cpu-installation.html
+        if [ ! -d vllm ]; then
+            git clone https://github.com/vllm-project/vllm
+        fi
+        pushd vllm
+            uv pip install wheel packaging ninja "setuptools>=49.4.0" numpy typing-extensions pillow setuptools-scm grpcio==1.66.2 protobuf bitsandbytes
+            uv pip install -v -r requirements-cpu.txt --extra-index-url https://download.pytorch.org/whl/cpu
+            VLLM_TARGET_DEVICE=cpu python setup.py install
+        popd
+        rm -rf vllm
+    else
+        installRequirements
+fi
