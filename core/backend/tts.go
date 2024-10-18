fb363bce08813f9a1e34fb364b8ca6f77f2353e9
Dave Lee <dave@gray101.com>
2024-10-17 20:36:38 -0400

    fix
    Signed-off-by: Dave Lee <dave@gray101.com>

--- core/backend/tts.go
+++ core/backend/tts.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/mudler/LocalAI/core/config"
+	"github.com/rs/zerolog/log"
 
 	"github.com/mudler/LocalAI/pkg/grpc/proto"
 	"github.com/mudler/LocalAI/pkg/model"
@@ -42,6 +43,8 @@ func ModelTTS(
 	if err := os.MkdirAll(appConfig.AudioDir, 0750); err != nil {
 		return "", nil, fmt.Errorf("failed creating audio directory: %s", err)
 	}
+
+	log.Warn().Str("config.Model", backendConfig.Model).Msg("ModelTTS before backend call")
 
 	fileName := utils.GenerateUniqueFileName(appConfig.AudioDir, "tts", ".wav")
 	filePath := filepath.Join(appConfig.AudioDir, fileName)
