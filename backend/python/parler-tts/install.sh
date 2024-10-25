3ad920b50a590d63c71deccbb2cc4e8ca537bec0
Ettore Di Giacinto <mudler@users.noreply.github.com>
2024-10-25 23:50:12 +0200

    fix(parler-tts): pin protobuf (#3963)
    * fix(parler-tts): pin protobuf
    
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * debug
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    * Re-apply workaround
    
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>
    
    ---------
    
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/parler-tts/install.sh
+++ backend/python/parler-tts/install.sh
@@ -11,4 +11,18 @@ if [ "x${BUILD_PROFILE}" == "xintel" ]; then
     EXTRA_PIP_INSTALL_FLAGS+=" --upgrade --index-strategy=unsafe-first-match"
 fi
 
+
 installRequirements
+
+
+# https://github.com/descriptinc/audiotools/issues/101
+# incompatible protobuf versions.
+PYDIR=python3.10
+pyenv="${MY_DIR}/venv/lib/${PYDIR}/site-packages/google/protobuf/internal/"
+
+if [ ! -d ${pyenv} ]; then
+    echo "(parler-tts/install.sh): Error: ${pyenv} does not exist"
+    exit 1
+fi
+
+curl -L https://raw.githubusercontent.com/protocolbuffers/protobuf/main/python/google/protobuf/internal/builder.py -o ${pyenv}/builder.py
