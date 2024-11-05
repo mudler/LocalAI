05225c93e4a0c3e10f95cfea2e6ae20d756d8255
Ettore Di Giacinto <mudler@localai.io>
2024-11-05 22:19:35 +0100

    Fix route
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- core/http/app.go
+++ core/http/app.go
@@ -90,7 +90,7 @@ func API(application *application.Application) (*fiber.App, error) {
 
 	router.Use(middleware.StripPathPrefix())
 
-	router.Use(func(c *fiber.Ctx) error {
+	router.Use("/v1/realtime", func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
 			// Returns true if the client requested upgrade to the WebSocket protocol
 			return c.Next()
