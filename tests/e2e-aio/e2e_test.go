695cbf58ab49b4dc1640b0bfdc65d57e591951a7
Dave Lee <dave@gray101.com>
2024-11-26 20:07:41 -0500

    reduce length of float32 samples in vad test
    Signed-off-by: Dave Lee <dave@gray101.com>

--- tests/e2e-aio/e2e_test.go
+++ tests/e2e-aio/e2e_test.go
@@ -243,7 +243,7 @@ var _ = Describe("E2E test", func() {
 				buf, err := d.FullPCMBuffer()
 				Expect(err).ToNot((HaveOccurred()))
 				fBuf := buf.AsFloat32Buffer().Data
-				fBuf = fBuf[:len(fBuf)/8] // Whole file is too long, attempting reduced length
+				fBuf = fBuf[:len(fBuf)/64] // Whole file is too long, attempting reduced length
 				req := schema.VADRequest{
 					BasicModelRequest: schema.BasicModelRequest{
 						Model: modelName,
