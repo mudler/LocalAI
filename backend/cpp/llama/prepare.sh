67786c9c412b0b57bf75f1c57ae07a2871467755
Ettore Di Giacinto <mudler@localai.io>
2025-05-16 22:01:38 +0200

    fix: copy json.hpp from the correct location
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/cpp/llama/prepare.sh
+++ backend/cpp/llama/prepare.sh
@@ -9,7 +9,7 @@ done
 
 cp -r CMakeLists.txt llama.cpp/tools/grpc-server/
 cp -r grpc-server.cpp llama.cpp/tools/grpc-server/
-cp -rfv llama.cpp/tools/server/json.hpp llama.cpp/tools/grpc-server/
+cp -rfv llama.cpp/common/json.hpp llama.cpp/tools/grpc-server/
 cp -rfv llama.cpp/tools/server/utils.hpp llama.cpp/tools/grpc-server/
 
 if grep -q "grpc-server" llama.cpp/tools/CMakeLists.txt; then
