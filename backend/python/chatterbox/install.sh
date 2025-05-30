d5c9c717b5b35aaea5aa3d8a22044fd3e41dbd8a
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-05-30 10:52:55 +0200

    feat(chatterbox): add new backend (#5524)
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

---
+++ backend/python/chatterbox/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+source $(dirname $0)/../common/libbackend.sh
+
+# This is here because the Intel pip index is broken and returns 200 status codes for every package name, it just doesn't return any package links.
+# This makes uv think that the package exists in the Intel pip index, and by default it stops looking at other pip indexes once it finds a match.
+# We need uv to continue falling through to the pypi default index to find optimum[openvino] in the pypi index
+# the --upgrade actually allows us to *downgrade* torch to the version provided in the Intel pip index
+if [ "x${BUILD_PROFILE}" == "xintel" ]; then
+    EXTRA_PIP_INSTALL_FLAGS+=" --upgrade --index-strategy=unsafe-first-match"
+fi
+
+installRequirements
