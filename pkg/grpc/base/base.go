2e1dc8deef4d3ef8b328d67bc72adb0270d24191
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-06-12 18:34:32 +0200

    Fix Typos in Comments and Error Messages (#5637)
    * Update initializers.go
    
    Signed-off-by: kilavvy <140459108+kilavvy@users.noreply.github.com>
    
    * Update base.go
    
    Signed-off-by: kilavvy <140459108+kilavvy@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: kilavvy <140459108+kilavvy@users.noreply.github.com>

--- pkg/grpc/base/base.go
+++ pkg/grpc/base/base.go
@@ -1,6 +1,6 @@
 package base
 
-// This is a wrapper to statisfy the GRPC service interface
+// This is a wrapper to satisfy the GRPC service interface
 // It is meant to be used by the main executable that is the server for the specific backend type (falcon, gpt3, etc)
 import (
 	"fmt"
