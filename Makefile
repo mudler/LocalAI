dd25082843365b83232f983ecc2bf71709e83c59
Ettore Di Giacinto <mudler@localai.io>
2025-07-11 19:28:32 +0200

    Try to run AIO tests
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- Makefile
+++ Makefile
@@ -470,6 +470,20 @@ run-e2e-image:
 	ls -liah $(abspath ./tests/e2e-fixtures)
 	docker run -p 5390:8080 -e MODELS_PATH=/models -e THREADS=1 -e DEBUG=true -d --rm -v $(TEST_DIR):/models --gpus all --name e2e-tests-$(RANDOM) localai-tests
 
+backends/llama-cpp: docker-build-llama-cpp docker-save-llama-cpp build-api
+	./local-ai backends install "ocifile://$(abspath ./backend-images/llama-cpp.tar)"
+
+docker-build-aio:
+	docker build --build-arg MAKEFLAGS="--jobs=5 --output-sync=target" -t local-ai:tests -f Dockerfile .
+	BASE_IMAGE=local-ai:tests DOCKER_AIO_IMAGE=local-ai-aio:test $(MAKE) docker-aio
+
+e2e-aio:
+	LOCALAI_BACKEND_DIR=$(abspath ./backends) \
+	LOCALAI_MODELS_DIR=$(abspath ./models) \
+	LOCALAI_IMAGE_TAG=test \
+	LOCALAI_IMAGE=local-ai-aio \
+	$(MAKE) run-e2e-aio
+
 run-e2e-aio: protogen-go
 	@echo 'Running e2e AIO tests'
 	$(GOCMD) run github.com/onsi/ginkgo/v2/ginkgo --flake-attempts $(TEST_FLAKES) -v -r ./tests/e2e-aio
--- backend/cpp/llama-cpp/run.sh
+++ backend/cpp/llama-cpp/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# Get the absolute current dir where the script is located
+CURDIR=$(dirname "$(realpath $0)")
+
 cd /
 
 echo "CPU info:"
@@ -12,34 +15,24 @@ BINARY=llama-cpp-fallback
 if grep -q -e "\savx\s" /proc/cpuinfo ; then
 	echo "CPU:    AVX    found OK"
 	BINARY=llama-cpp-avx
-else
-	echo "CPU: no AVX    found"
-	BINARY=llama-cpp-fallback
 fi
 
 if grep -q -e "\savx2\s" /proc/cpuinfo ; then
 	echo "CPU:    AVX2   found OK"
 	BINARY=llama-cpp-avx2
-else
-	echo "CPU: no AVX2   found"
-	BINARY=llama-cpp-fallback
 fi
 
 # Check avx 512
-if grep -q -e "\savx512\s" /proc/cpuinfo ; then
-	echo "CPU:    AVX512 found OK"
+if grep -q -e "\savx512f\s" /proc/cpuinfo ; then
+	echo "CPU:    AVX512F found OK"
 	BINARY=llama-cpp-avx512
-else
-	echo "CPU: no AVX512 found"
-	BINARY=llama-cpp-fallback
 fi
 
 if [ -n "$LLAMACPP_GRPC_SERVERS" ]; then
 	BINARY=llama-cpp-grpc
 fi
-
+ 
 # Extend ld library path with the dir where this script is located/lib
-CURDIR=$(dirname "$0")
 if [ "$(uname)" == "Darwin" ]; then
 	DYLD_FALLBACK_LIBRARY_PATH=$CURDIR/lib:$DYLD_FALLBACK_LIBRARY_PATH
 else
@@ -48,8 +41,10 @@ fi
 
 # If there is a lib/ld.so, use it
 if [ -f $CURDIR/lib/ld.so ]; then
-	BINARY="$CURDIR/lib/ld.so $BINARY"
+	echo "Using lib/ld.so"
+	echo "Using binary: $BINARY"
+	$CURDIR/lib/ld.so $CURDIR/$BINARY "$@"
 fi
 
 echo "Using binary: $BINARY"
-exec ./$BINARY "$@"
+exec $CURDIR/$BINARY "$@"
