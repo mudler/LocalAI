455b2920a56b396722c21c193159bed5d713ec1f
Richard Palethorpe <io@richiejp.com>
2025-08-07 13:19:18 +0100

    Try converting SD to purego

--- backend/go/stablediffusion-ggml/gosd.h
+++ backend/go/stablediffusion-ggml/gosd.h
@@ -1,8 +1,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-int load_model(char *model, char *model_path, char* options[], int threads, int diffusionModel);
-int gen_image(char *text, char *negativeText, int width, int height, int steps, int seed, char *dst, float cfg_scale, char *src_image, float strength, char *mask_image, char **ref_images, int ref_images_count);
+int load_model(const char *model, char *model_path, char* options[], int threads, int diffusionModel);
+int gen_image(char *text, char *negativeText, int width, int height, int steps, int64_t seed, char *dst, float cfg_scale, char *src_image, float strength, char *mask_image, char **ref_images, int ref_images_count);
 #ifdef __cplusplus
 }
 #endif
