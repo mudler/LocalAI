70f7d0c25facbd1174ff84efdd060015c7e1b1c1
Ettore Di Giacinto <mudler@users.noreply.github.com>
2025-08-15 15:18:40 +0200

    Revert "chore(build): Convert stablediffusion-ggml backend to Purego (#5989)" (#6064)
    This reverts commit 94cb20ae7f864c1881b9d55e6af283a8453ba4fc.

--- backend/go/stablediffusion-ggml/main.go
+++ backend/go/stablediffusion-ggml/main.go
@@ -3,10 +3,7 @@ package main
 // Note: this is started internally by LocalAI and a server is allocated for each model
 import (
 	"flag"
-	"fmt"
-	"runtime"
 
-	"github.com/ebitengine/purego"
 	grpc "github.com/mudler/LocalAI/pkg/grpc"
 )
 
@@ -14,24 +11,7 @@ var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
-func getLibrary() string {
-	switch runtime.GOOS {
-	case "linux":
-		return "./libgosd.so"
-	default:
-		panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
-	}
-}
-
 func main() {
-	gosd, err := purego.Dlopen(getLibrary(), purego.RTLD_NOW|purego.RTLD_GLOBAL)
-	if err != nil {
-		panic(err)
-	}
-
-	purego.RegisterLibFunc(&LoadModel, gosd, "load_model")
-	purego.RegisterLibFunc(&GenImage, gosd, "gen_image")
-
 	flag.Parse()
 
 	if err := grpc.StartServer(*addr, &SDGGML{}); err != nil {
