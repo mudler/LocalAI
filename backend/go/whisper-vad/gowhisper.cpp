bf1e9a47296579d1e1e69daf66c3faa3a7891992
Richard Palethorpe <io@richiejp.com>
2025-08-19 17:24:12 +0100

    whisper-vad purego test

--- backend/go/whisper-vad/gowhisper.cpp
+++ backend/go/whisper-vad/gowhisper.cpp
@@ -1,8 +1,10 @@
 #include "ggml-backend.h"
 #include "whisper.h"
+#include <vector>
 #include "gowhisper.h"
 
 static struct whisper_vad_context *vctx;
+static std::vector<float> flat_segs;
 
 int load_model(const char *const model_path) {
   ggml_backend_load_all();
@@ -11,7 +13,7 @@ int load_model(const char *const model_path) {
   struct whisper_vad_context_params vcparams = whisper_vad_default_context_params();
 
   // XXX: Overridden to false in upstream due to performance?
-  vcparams.use_gpu = true;
+  // vcparams.use_gpu = true;
 
   vctx = whisper_vad_init_from_file_with_params(model_path, vcparams);
   if (vctx == nullptr) {
@@ -22,6 +24,27 @@ int load_model(const char *const model_path) {
   return 0;
 }
 
-int vad() {
+int vad(float pcmf32[], size_t pcmf32_size, float **segs_out, size_t *segs_out_len) {
+  if (!whisper_vad_detect_speech(vctx, pcmf32, pcmf32_size)) {
+    fprintf(stderr, "error: failed to detect speech\n");
+    return 1;
+  }
+
+  struct whisper_vad_params params = whisper_vad_default_params();
+  struct whisper_vad_segments *segs = whisper_vad_segments_from_probs(vctx, params);
+  size_t segn = whisper_vad_segments_n_segments(segs);
+
+  flat_segs.clear();
+
+  for (int i = 0; i < segn; i++) {
+    flat_segs.push_back(whisper_vad_segments_get_segment_t0(segs, i));
+    flat_segs.push_back(whisper_vad_segments_get_segment_t1(segs, i));
+  }
+
+  *segs_out = flat_segs.data();
+  *segs_out_len = flat_segs.size();
+
+  whisper_vad_free_segments(segs);
+
   return 0;
 }
--- backend/go/whisper-vad/gowhisper.go
+++ backend/go/whisper-vad/gowhisper.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 
 	"github.com/mudler/LocalAI/pkg/grpc/base"
 	pb "github.com/mudler/LocalAI/pkg/grpc/proto"
@@ -9,7 +10,7 @@ import (
 
 var (
 	CppLoadModel func(modelPath string) int
-	CppVAD func() int
+	CppVAD func(pcmf32 []float32, pcmf32Size uintptr, segsOut unsafe.Pointer, segsOutLen unsafe.Pointer) int
 )
 
 type Whisper struct {
@@ -18,7 +19,7 @@ type Whisper struct {
 }
 
 func (w *Whisper) Load(opts *pb.ModelOptions) error {
-	if ret := CppLoadModel(opts.ModelPath); ret != 0 {
+	if ret := CppLoadModel(opts.ModelFile); ret != 0 {
 		return fmt.Errorf("Failed to load VAD model")
 	}
 
@@ -26,7 +27,22 @@ func (w *Whisper) Load(opts *pb.ModelOptions) error {
 }
 
 func (w *Whisper) VAD(req *pb.VADRequest) (pb.VADResponse, error) {
+	audio := req.Audio
+	var segsPtr, segsLen uintptr
+
+	if ret := CppVAD(audio, uintptr(len(audio)), unsafe.Pointer(&segsPtr), unsafe.Pointer(&segsLen)); ret != 0 {
+		return pb.VADResponse{}, fmt.Errorf("Failed VAD")
+	}
+
+	segs := (*(*[1 << 30]float32)(unsafe.Pointer(segsPtr)))[:segsLen:segsLen]
+
 	vadSegments := []*pb.VADSegment{}
+	for i := range len(segs) >> 1 {
+		vadSegments = append(vadSegments, &pb.VADSegment{
+			Start: segs[2*i],
+			End: segs[2*i + 1],
+		})
+	}
 
 	return pb.VADResponse{
 		Segments: vadSegments,
