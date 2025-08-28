e6ebfd3ba19d1bc749fa046ada2055341602a915
Richard Palethorpe <io@richiejp.com>
2025-08-28 16:25:18 +0100

    feat(whisper-cpp): Convert to Purego and add VAD (#6087)
    * fix(ci): Avoid matching wrong backend with the same prefix
    
    Signed-off-by: Richard Palethorpe <io@richiejp.com>
    
    * chore(whisper): Use Purego and enable VAD
    
    This replaces the Whisper CGO bindings with our own Purego based module
    to make compilation easier.
    
    In addition this allows VAD models to be loaded by Whisper. There is not
    much benefit now except that the same backend can be used for VAD and
    transcription. Depending on upstream we may also be able to use GPU for
    VAD in the future, but presently it is disabled.
    
    Signed-off-by: Richard Palethorpe <io@richiejp.com>
    
    ---------
    
    Signed-off-by: Richard Palethorpe <io@richiejp.com>
    Co-authored-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

--- backend/go/whisper/main.go
+++ backend/go/whisper/main.go
@@ -1,10 +1,10 @@
 package main
 
 // Note: this is started internally by LocalAI and a server is allocated for each model
-
 import (
 	"flag"
 
+	"github.com/ebitengine/purego"
 	grpc "github.com/mudler/LocalAI/pkg/grpc"
 )
 
@@ -12,7 +12,33 @@ var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
+type LibFuncs struct {
+	FuncPtr any
+	Name    string
+}
+
 func main() {
+	gosd, err := purego.Dlopen("./libgowhisper.so", purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	if err != nil {
+		panic(err)
+	}
+
+	libFuncs := []LibFuncs{
+		{&CppLoadModel, "load_model"},
+		{&CppLoadModelVAD, "load_model_vad"},
+		{&CppVAD, "vad"},
+		{&CppTranscribe, "transcribe"},
+		{&CppGetSegmentText, "get_segment_text"},
+		{&CppGetSegmentStart, "get_segment_t0"},
+		{&CppGetSegmentEnd, "get_segment_t1"},
+		{&CppNTokens, "n_tokens"},
+		{&CppGetTokenID, "get_token_id"},
+	}
+
+	for _, lf := range libFuncs {
+		purego.RegisterLibFunc(lf.FuncPtr, gosd, lf.Name)
+	}
+
 	flag.Parse()
 
 	if err := grpc.StartServer(*addr, &Whisper{}); err != nil {
