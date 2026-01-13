914a7f2cae0280d71ea486027e76c3b7ee6b4af5
Ettore Di Giacinto <mudler@localai.io>
2026-01-13 21:12:28 +0000

    feat(pocket-tts): add new backend
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

---
+++ backend/python/pocket-tts/protogen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+backend_dir=$(dirname $0)
+if [ -d $backend_dir/common ]; then
+    source $backend_dir/common/libbackend.sh
+else
+    source $backend_dir/../common/libbackend.sh
+fi
+
+python3 -m grpc_tools.protoc -I../.. -I./ --python_out=. --grpc_python_out=. backend.proto
