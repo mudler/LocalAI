241bd490523720f9cd817bec2d4ee25e8c223b56
Ettore Di Giacinto <mudler@localai.io>
2026-01-24 20:11:27 +0000

    default to py3.12
    Signed-off-by: Ettore Di Giacinto <mudler@localai.io>

--- backend/python/vllm-omni/install.sh
+++ backend/python/vllm-omni/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+PYTHON_VERSION="3.12"
+PYTHON_PATCH="12"
+PY_STANDALONE_TAG="20251120"
+
 backend_dir=$(dirname $0)
 if [ -d $backend_dir/common ]; then
     source $backend_dir/common/libbackend.sh
